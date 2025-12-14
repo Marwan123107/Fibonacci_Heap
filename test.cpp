7fad0d6d2bb1cc9985c696c8ee64a05de2ebd479
Marwan123107 <marwandraz3@gmail.com>
2025-12-14 23:23:45 +0200

    ggg

--- test.cpp
+++ test.cpp
@@ -3,15 +3,5 @@
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
-  
-cout<<"Yarab Elnaga7";
-    
-=======
-    cout << "Hello data structures!" << endl;
 
-    // 3dlt 3aleh 2aho
-    cout<<"MANGAAAAAAaa"<<endl;
-    return 0;
->>>>>>> 6632e402b7bad729b0a4bc50e1e7c10315b28c34
 }
